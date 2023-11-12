c25d0689b545f0f3548c5689e6e38f1576c276d4
Can Kayser <cankayser@Cans-Air.fritz.box>
2023-11-12 15:05:40 +0100

    Add early stopping

--- regularize.py
+++ regularize.py
@@ -2,7 +2,6 @@
 from tools.plotting import  eval_and_plot
 from tools.data import DataHandler
 import torch
-from numpy import Inf
 from param_search import data_handler
 
 # SNR = 1.
@@ -13,12 +12,13 @@ MODELFILE = "mymodel.torch"
 LAMBDAS = [9., 5., 1., .5, .1, 0.]
 LEARNING_RATES = [.1, .01, .001, .0001, .00001]
 EPOCHS = 500
+PATIENCE = 50
 
 ADJUST_LR = [.1, .3, .5, .7 , 1., 2., 3., 4.]
 
 best_lr = None
 best_dim = None
-min_error = Inf
+min_error = float('inf')
 for lam in LAMBDAS:
 
     print(f'Regularization Paramter: {lam}')
@@ -30,7 +30,7 @@ for lam in LAMBDAS:
         model.hyperparams.criterion = new_criterion
         model.hyperparams.learning_rate = lr
         model.hyperparams.epochs = EPOCHS
-        model.start_training(data_handler, verbosity=0)
+        model.start_training(data_handler, verbosity=0, patience=PATIENCE)
 
         model.hyperparams.criterion = criterion
         _,_, testlosses = model.start_evaluation(data_handler)
@@ -46,7 +46,7 @@ for lam in LAMBDAS:
 print(f'Best Regularization Paramter: {best_lam} \t \t Best Learning Rate: {best_lr}')
 
 very_best_lr = None
-min_error = Inf
+min_error = float('inf')
 for adjust_lr in ADJUST_LR:
     data_handler.reset()
     model = torch.load(MODELFILE)
@@ -55,7 +55,7 @@ for adjust_lr in ADJUST_LR:
     model.hyperparams.criterion = new_criterion
     model.hyperparams.learning_rate = lr
     model.hyperparams.epochs = 2*EPOCHS
-    model.start_training(data_handler, verbosity=0)
+    model.start_training(data_handler, verbosity=0, patience=PATIENCE)
 
     model.hyperparams.criterion = criterion
     _,_, testlosses = model.start_evaluation(data_handler)
@@ -72,7 +72,7 @@ new_criterion = lambda x,y: criterion(x,y) + best_lam * model.sum_weights()
 model.hyperparams.criterion = new_criterion
 model.hyperparams.learning_rate = very_best_lr
 model.hyperparams.epochs = 2*EPOCHS
-model.start_training(data_handler, verbosity=2)
+model.start_training(data_handler, verbosity=2, patience=PATIENCE)
 
 model.hyperparams.criterion = criterion
 _,_, testlosses = model.start_evaluation(data_handler)
