e8cb7f7fcbfa1963d748a6f86dd3d3e389e53a2b
Can Kayser <cankayser@eduroam-141-23-220-208.wlan.tu-berlin.de>
2023-11-14 15:19:49 +0100

    Rename target function

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 from tools.hyperparameters import Hyperparameters
 from tools.model import MLP
 from tools.data import DataHandler
+from tools.plotting import eval_and_plot
 
 hyperparams = Hyperparameters(hidden_dim=3)
 data_handler = DataHandler()
 model = MLP(hyperparams=hyperparams)
 model.start_training(data_handler)
+eval_and_plot(model, data_handler)
 # %%
