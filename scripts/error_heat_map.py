a56b9457d45d13b4a979a4c3fe745e3e5ce71ea2
Can Kayser <cankayser@eduroam-141-23-196-165.wlan.tu-berlin.de>
2023-12-18 12:21:05 +0100

    Add heatmap script

---
+++ scripts/error_heat_map.py
@@ -0,0 +1,40 @@
+#%%
+import pathsettings
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from tools.hyperparameters import Hyperparameters
+from tools.data import MaterialDataHandler
+from tools.model import MLP
+from scripts.fit_material import (MATERIAL, DEFAULT_DEFORMATION_FUNCTION,INPUT_DIM,
+                                  PATIENCE, HIDDEN_DIMS, N_LAYERS)
+from scripts.param_search import parameter_search
+
+import torch
+
+EPOCHS=250
+
+torch.random.manual_seed(1)
+data_handler = MaterialDataHandler(material=MATERIAL,
+                                    deformation_function=DEFAULT_DEFORMATION_FUNCTION,
+                                    max_body_scale=1, batchsize=16, samples=1,
+                                    body_resolution=10)
+
+hyperparams = Hyperparameters(input_dim=INPUT_DIM, n_layers=N_LAYERS)
+hyperparams = Hyperparameters(input_dim=INPUT_DIM, n_layers=N_LAYERS, epochs=EPOCHS, hidden_dim=55)
+model = MLP(hyperparams=hyperparams)
+
+model.start_training(data_handler, verbosity=2)
+
+
+#%%
+model, error_heat_map = parameter_search(model, data_handler, epochs=EPOCHS, patience=PATIENCE,
+                            hidden_dimensions=HIDDEN_DIMS, verbosity=1)
+
+df = pd.DataFrame(error_heat_map)
+sns.heatmap(df, annot=True, fmt='.3g', cbar_kws={'label': 'MSE'}, cmap='Oranges')
+plt.xlabel('Hidden Dimension')
+plt.ylabel('Learning Rate')
+plt.savefig('ignore/heatmap')
+
+# %%
